1b392e95692f79bc016a534244b968756eacec46
Denis Gavrilenko <dgavrilenko@wanna.by>
2019-11-17 18:31:40 +0300

    [xcode] add project

---
+++ weatherapp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  weatherapp
+//
+//  Created by Denis Gavrilenko on 11/17/19.
+//  Copyright © 2019 Denis. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ weatherapp/WeatherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.swift
+//  weatherapp
+//
+//  Created by Denis Gavrilenko on 11/17/19.
+//  Copyright © 2019 Denis. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+}
+
